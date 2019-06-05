ebd69281eef3704a928f5107291557c3ab90347e
WangRunji <wangrunji0408@163.com>
2019-06-06 01:41:58 +0800

    fix multiple BGM. convert 奏折框 from png to jpg

--- assets/Script/MainUIController.ts
+++ assets/Script/MainUIController.ts
@@ -8,10 +8,15 @@ export default class MainUIController extends cc.Component {
     @property(cc.AudioClip)
     bgm: cc.AudioClip = null;
 
-    onLoad() {
-        cc.game.setFrameRate(30);
-        UserData.load();
-        cc.audioEngine.play(this.bgm, true, 0.5);
+    static loaded: boolean = false;
+
+    start() {
+        if(!MainUIController.loaded) {
+            cc.game.setFrameRate(30);
+            UserData.load();
+            cc.audioEngine.play(this.bgm, true, 0.5);
+            MainUIController.loaded = true;
+        }
     }
 
     onButtonClicked(_event: TouchEvent, customEventData: string) {
